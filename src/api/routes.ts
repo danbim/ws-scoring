18c191ea20d9f0514e51459090dd54424caf5b61
Daniel Bimschas <daniel@bimschas.com>
2025-12-21 09:04:36 +0100

    Add todo

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -114,6 +114,7 @@ async function processCommand(command: HeatCommand): Promise<Response> {
   const heatId = command.data.heatId;
   const events = await handleCommand(command);
 
+  // TODO: this probably is not super reliable and produces latency. find a better way to do this.
   // Broadcast events to WebSocket clients
   for (const event of events) {
     await broadcastEvent(heatId, event);
