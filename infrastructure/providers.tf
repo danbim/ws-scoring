43343389587b1f56a6a35a1488529c34ab99a63a
Daniel Bimschas <daniel@bimschas.com>
2026-01-09 13:27:55 +0100

    (ci): remove duplicate provider config

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -18,8 +18,6 @@ terraform {
 }
 
 provider "scaleway" {
-  access_key      = var.scw_access_key
-  secret_key      = var.scw_secret_key
   project_id      = var.scw_project_id
   organization_id = var.scw_organization_id
   region          = var.region
