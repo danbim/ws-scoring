78440e67b8947ff6dfb0a153956e69c16d9b59f8
Daniel Bimschas <daniel@bimschas.com>
2026-01-10 14:37:58 +0100

    (chore) format

--- src/app/pages/HeatScoreSheet.tsx
+++ src/app/pages/HeatScoreSheet.tsx
@@ -440,7 +440,9 @@ const HeatScoreSheet: Component<HeatScoreSheetProps> = (props) => {
                 </div>
 
                 {/* Finish Heat Button (Head Judge Only) */}
-                <Show when={(auth.isHeadJudgeOrAdmin() || auth.isJudge()) && !currentHeat().completedAt}>
+                <Show
+                  when={(auth.isHeadJudgeOrAdmin() || auth.isJudge()) && !currentHeat().completedAt}
+                >
                   <div class="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 p-4">
                     <button
                       type="button"
